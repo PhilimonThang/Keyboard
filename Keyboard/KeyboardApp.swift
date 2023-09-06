f11aa35ab0e4ab0213dc1f936844dd5d87bcb071
PhilT <thang.zoramthanga@gmail.com>
2023-09-06 00:18:05 -0400

    Initial Commit

---
+++ Keyboard/KeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardApp.swift
+//  Keyboard
+//
+//  Created by macos on 9/6/23.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
